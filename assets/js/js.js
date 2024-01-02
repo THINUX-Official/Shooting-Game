e99f5689411a50b9b20da382b9cfe18c767f5a64
THINUX-Official <thinuxofficial@gmail.com>
2024-01-02 15:47:11 +0530

    Shooting sound fixed.

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,48 +1,49 @@
-/*all the function control through here*/
-$(".area").on("click", function () {
-    faceSpawns();
+// all the function control through here
+$(".area").click(function () {
     gunAnimation();
     sound();
+    faceSpawns();
+    sound(); // calling twice this method is fixed the issue.
 });
 
-/*sound animation*/
+// sound animation
 function sound() {
-    let audio = document.getElementById("audio");
-    // let gunImage = document.getElementById("gunImage");
+    let audio = $("#audio")[0];
 
     if (audio.paused) {
         audio.currentTime = 0;
         audio.play();
         // Remove the "ended" event listener after the first play
-        audio.removeEventListener("ended", loopAudio);
+        audio.removeEventListener("ended");
     } else {
         audio.pause();
     }
 }
 
-/*gun animation*/
-
+// gun animation
 let gunFire = ["assets/img/pistol/1.png", "assets/img/pistol/2.png", "assets/img/pistol/3.png", "assets/img/pistol/4.png", "assets/img/pistol/5.png"];
 
 let currentIndex = 0;
-let gunImage = document.getElementById("gunImage");
+// let gunImage = document.getElementById("gunImage");
+let gunImage = $("#gunImage")[0];
 
 function gunAnimation() {
-    let interval = setInterval(() => {
+
+    let interval = setInterval(function () {
         gunImage.src = gunFire[currentIndex];
         currentIndex = (currentIndex + 1) % gunFire.length;
     }, 100); // Time interval between each image change (adjust as needed)
 
     // After some time, stop the animation and show the default image
-    setTimeout(() => {
+    setTimeout(function () {
         clearInterval(interval);
         gunImage.src = "assets/img/pistol/1.png"; // Set the default image
-    }, 500); // Total animation time (adjust as needed)
+
+    }, 500);
 }
 
 
-/*face spawns*/
-
+// face spawns
 let start = new Date().getTime();
 let end = new Date().getTime();
 let time = ((end - start) / 1000); // execution time
@@ -102,62 +103,60 @@ function faceSpawns() {
 }
 
 
-/*one-minute timer*/
-$(document).ready(function () { // ensure, code that inside this function will load, when the HTML document fully loaded.
+// one-minute timer
+$("#timer").text("00:00"); // display timer before start.
 
-    $("#timer").text("00:00"); // display timer before start.
+let timeInterval;
+let remainingTime = 0;
+let type = 'start'; // toggle variable(change values) for the button, and it will hold values such as start, pause and resume.
 
-    let timeInterval;
-    let remainingTime = 0;
-    let type = 'start'; // toggle variable(change values) for the button, and it will hold values such as start, pause and resume.
+function startTimer(duration, display) { // countdown and update the display.
 
-    function startTimer(duration, display) { // countdown and update the display.
+    clearInterval(timeInterval); // check if timeout is running, it will be closed first.
 
-        clearInterval(timeInterval); // check if timeout is running, it will be closed first.
+    let timer = duration;
 
-        let timer = duration;
+    timeInterval = setInterval(function () {
+        // pastInt function use to convert result to integer.
+        // The second argument 10 specifies the radix or base (decimal in this case).
+        let minutes = parseInt(timer / 60, 10); // if timer is 120 seconds, then minutes would be 2 (120 / 60 = 2).
+        let seconds = parseInt(timer % 60, 10); // if timer is 120 seconds, then seconds would be 0 (120 % 60 = 0).
 
-        timeInterval = setInterval(function () {
-            // pastInt function use to convert result to integer.
-            // The second argument 10 specifies the radix or base (decimal in this case).
-            let minutes = parseInt(timer / 60, 10); // if timer is 120 seconds, then minutes would be 2 (120 / 60 = 2).
-            let seconds = parseInt(timer % 60, 10); // if timer is 120 seconds, then seconds would be 0 (120 % 60 = 0).
+        minutes = minutes < 10 ? "0" + minutes : minutes; // if minutes is 5, then formattedMinutes would be "05". If minutes is 15, then formattedMinutes would be "15".
+        seconds = seconds < 10 ? "0" + seconds : seconds; //  if seconds is 3, then formattedSeconds would be "03". If seconds is 25, then formattedSeconds would be "25".
 
-            minutes = minutes < 10 ? "0" + minutes : minutes; // if minutes is 5, then formattedMinutes would be "05". If minutes is 15, then formattedMinutes would be "15".
-            seconds = seconds < 10 ? "0" + seconds : seconds; //  if seconds is 3, then formattedSeconds would be "03". If seconds is 25, then formattedSeconds would be "25".
+        // updating text content of the display element with formatted minutes and seconds.
+        display.text(minutes + ":" + seconds); // if formattedMinutes is "05" and formattedSeconds is "30", the result would be "05:30".
 
-            // updating text content of the display element with formatted minutes and seconds.
-            display.text(minutes + ":" + seconds); // if formattedMinutes is "05" and formattedSeconds is "30", the result would be "05:30".
-
-            if (--timer < 0) {
-                clearInterval(timeInterval); //  stops the timer from counting down further.
-                timer = duration; //  restarts the timer, allowing it to count down again from the specified duration.
-                $("#start").text("START");
-            }
-        }, 1000); // 1000 milliseconds (1 second).
-    }
-
-    let oneMinute = 60; // set the duration of the timer to one minute.
-    let display = $("#timer"); // select the element with the id "timer".
-
-    $("#start").click(function () {
-        if (type === 'start') {
-            // start timer
-            startTimer(oneMinute, display);
-            type = 'pause';
-            $("#start").text("PAUSE");
-
-        } else if (type === 'pause') {
-            type = 'resume';
-            $("#start").text("RESUME");
-            clearInterval(timeInterval);
-            remainingTime = oneMinute - ($("#timer").text().split(':')[0] * 60 + parseInt($("#timer").text().split(':')[1]));
-
-        } else if (type === 'resume') {
-            type = 'pause';
-            $("#start").text("PAUSE");
-            startTimer(oneMinute - remainingTime, display);
+        if (--timer < 0) {
+            clearInterval(timeInterval); //  stops the timer from counting down further.
+            timer = duration; //  restarts the timer, allowing it to count down again from the specified duration.
+            $("#start").text("START");
         }
-    });
-});
+    }, 1000); // 1000 milliseconds (1 second).
+}
 
+let oneMinute = 60; // set the duration of the timer to one minute.
+let display = $("#timer"); // select the element with the id "timer".
+
+
+// start button.
+$("#start").click(function () {
+    if (type === 'start') {
+        // start timer
+        startTimer(oneMinute, display);
+        type = 'pause';
+        $("#start").text("PAUSE");
+
+    } else if (type === 'pause') {
+        type = 'resume';
+        $("#start").text("RESUME");
+        clearInterval(timeInterval);
+        remainingTime = oneMinute - ($("#timer").text().split(':')[0] * 60 + parseInt($("#timer").text().split(':')[1]));
+
+    } else if (type === 'resume') {
+        type = 'pause';
+        $("#start").text("PAUSE");
+        startTimer(oneMinute - remainingTime, display);
+    }
+});
